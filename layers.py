63f359502d2342e6852124bac2747e8727083b74
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-09-18 17:18:30 +0530

    add layers.py

---
+++ layers.py
@@ -0,0 +1,10 @@
+from torch import nn
+
+class InputEmbeddingLayer(nn.Module):
+    """Input Embedding layer used by QANet
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(InputEmbeddingLayer, self).__init__()

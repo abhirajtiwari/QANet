ca8591d7a788416b0e9995d2010a82d00c15caf4
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-09-18 18:04:38 +0530

    add structure

--- layers.py
+++ layers.py
@@ -1,5 +1,8 @@
 from torch import nn
 
+
+# --------------- Model Layers ------------------
+
 class InputEmbeddingLayer(nn.Module):
     """Input Embedding layer used by QANet
 
@@ -15,3 +18,81 @@ class InputEmbeddingLayer(nn.Module):
 
         return emb
 
+class EmbeddingEncodeLayer(nn.Module):
+    """[summary]
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(EmbeddingEncoderLayer, self).__init__()
+
+    def forward(self, x):
+
+        return out
+
+class CQAttentionLayer(nn.Module):
+    """[summary]
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(CQAttentionLayer, self).__init__()
+
+    def forward(self, context, question):
+
+        return out
+
+class ModelEncoderLayer(nn.Module):
+    """[summary]
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(ModelEncoderLayer, self).__init__()
+
+    def forward(self, x):
+
+        return out
+
+class OutputLayer(nn.Module):
+    """Takes inputs from 2 of the ModelEncoderLayers
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(OutputLayer, self).__init__()
+
+    def forward(self, a, b):
+
+        return out
+
+# ---------------- Helper Layers ----------------------        
+
+class SelfAttention(nn.Module):
+    """[summary]
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(SelfAttention, self).__init__()
+    def forward(self, x):
+
+        return out
+
+class EncoderBlock(nn.Module):
+    """[summary]
+
+    Args:
+        nn ([type]): [description]
+    """
+    def __init__(self, ):
+        super(EmbeddingEncoderLayer, self).__init__()
+
+    def forward(self, x):
+        
+        return out

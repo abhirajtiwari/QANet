2f42b82c6178d8a9aad567fa31a5c4a19b3bb7c8
Sahil Khose <sahilkhose18@gmail.com>
2020-09-18 20:32:26 +0530

    Update model-encoder layer parameters

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import torch.nn.Functional as F
 class QANET(nn.module()):
     """Implementation of QANET model for SQuAD 2.0
     """
-    def __init__(self, word_vectors, hidden_size, drop_prob=0.):
+    def __init__(self, word_vectors, hidden_size=128, drop_prob=0.):
         super(QANET, self).__init__()
         self.emb = layers.InputEmbeddingLayer(
             word_vectors=word_vectors,
@@ -26,7 +26,7 @@ class QANET(nn.module()):
         )
         self.mod_enc = layers.ModelEncoderLayer(
             conv_layers=2,
-            kernel_size=7,
+            kernel_size=5,
             filters=128,
             heads=8,
             enc_blocks=7,

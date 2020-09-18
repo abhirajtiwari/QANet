980edbd01389850ff4ec0eb42c639416d2a20ac6
Sahil Khose <sahilkhose18@gmail.com>
2020-09-18 20:17:20 +0530

    Add embedding and encoder layers

--- layers.py
+++ layers.py
@@ -1,107 +1,120 @@
 import torch
-from torch import nn
+import torch.nn as nn
 import torch.nn.Functional as F
 
 
 # --------------- Model Layers ------------------
 
 class InputEmbeddingLayer(nn.Module):
-    """Input Embedding layer used by QANet
-    Word embeddings of 300 D
-
-    Args:
-        word_vectors (torch.Tensor): GLoVE vectors (300-D)
     """
-    def __init__(self, word_vectors):
+    Class that converts input words to their 300-d GLoVE word embeddings.
+    """
+    def __init__(self, word_vectors, hidden_size, drop_prob):
+        """
+        output shape: (batch_size, seq_len, embed_size=300)
+        """
         super(InputEmbeddingLayer, self).__init__()
-        self.embedding = nn.Embedding.from_pretrained(word_vectors)
+        self.word_embed_size = word_embed_size
+        self.drop_prob = drop_prob 
+        self.hidden_size = hidden_size
+
+        self.embed = nn.Embedding.from_pretrained(word_vectors)
+        
 
     def forward(self, x):
-        emb = self.embedding(x) # (batch_size, sequence_length, p1=300)
+        emb = self.embed(x)   # (batch_size, seq_len, embed_size)
+        emb = F.dropout(emb, self.drop_prob, self.training)
 
         return emb
 
-class EmbeddingEncodeLayer(nn.Module):
-    """[summary]
-
-    Args:
-        nn ([type]): [description]
+class EmbeddingEncoderLayer(nn.Module):
     """
-    def __init__(self, ):
+    output shape: (batch_size, seq_len, enc_size=128)
+    """
+
+    def __init__(self, conv_layers, kernel_size, filters, heads, enc_blocks, drop_prob):
         super(EmbeddingEncoderLayer, self).__init__()
+        self.emb_enc = EncoderBlock(
+            conv_layers=conv_layers, 
+            kernel_size=kernel_size,
+            filters=filters,
+            heads=heads, 
+            drop_prob=drop_prob)
 
     def forward(self, x):
-
+        out = self.emb_enc(x)
         return out
 
 
 class CQAttentionLayer(nn.Module):
-    """[summary]
-
-    Args:
-        nn ([type]): [description]
     """
-    def __init__(self, ):
+    """
+    def __init__(self, drop_prob):
         super(CQAttentionLayer, self).__init__()
 
     def forward(self, context, question):
-
-        return out
+        pass
+        # return out
 
 
 class ModelEncoderLayer(nn.Module):
-    """[summary]
-
-    Args:
-        nn ([type]): [description]
     """
-    def __init__(self, ):
+    """
+
+    def __init__(self, conv_layers, kernel_size, filters, heads, enc_blocks, drop_prob):
         super(ModelEncoderLayer, self).__init__()
+        self.model_enc = nn.ModuleList([
+            EncoderBlock(
+                conv_layers=conv_layers,
+                kernel_size=kernel_size,
+                filters=filters,
+                heads=heads,
+                drop_prob=drop_prob)
+            for _ in range(enc_blocks)])
 
     def forward(self, x):
-
+        out = self.model_enc(x)
         return out
 
 
 class OutputLayer(nn.Module):
-    """Takes inputs from 2 of the ModelEncoderLayers
-
-    Args:
-        nn ([type]): [description]
     """
-    def __init__(self, ):
+    """
+    def __init__(self, drop_prob):
         super(OutputLayer, self).__init__()
 
     def forward(self, a, b):
-
-        return out
+        pass
+        # return out
 
 
 # ---------------- Helper Layers ----------------------        
 
 class SelfAttention(nn.Module):
-    """[summary]
-
-    Args:
-        nn ([type]): [description]
     """
-    def __init__(self, ):
+    """
+    def __init__(self, heads, drop_prob):
         super(SelfAttention, self).__init__()
         
     def forward(self, x):
-
-        return out
+        pass
+        # return out
 
 
 class EncoderBlock(nn.Module):
-    """[summary]
-
-    Args:
-        nn ([type]): [description]
     """
-    def __init__(self, ):
-        super(EmbeddingEncoderLayer, self).__init__()
+    """
+
+    def __init__(self, conv_layers, kernel_size, filters, heads, drop_prob):
+        super(EncoderBlock, self).__init__()
+
+        self.conv = nn.ModuleList([nn.Conv2d(in_channels=_, out_channels=_, kernel_size=kernel_size)
+                                   for _ in range(conv_layers)])
+        self.att = SelfAttention(heads=heads, drop_prob=drop_prob)
+        self.ff = nn.Linear()
 
     def forward(self, x):
-        
+        x = self.conv(x)
+        x = self.att(x)
+        out = self.ff(x)
         return out
---
+++ models.py
@@ -0,0 +1,40 @@
+import layers
+import torch
+import torch.nn as nn
+import torch.nn.Functional as F 
+
+class QANET(nn.module()):
+    """Implementation of QANET model for SQuAD 2.0
+    """
+    def __init__(self, word_vectors, hidden_size, drop_prob=0.):
+        super(QANET, self).__init__()
+        self.emb = layers.InputEmbeddingLayer(
+            word_vectors=word_vectors,
+            hidden_size=hidden_size,
+            drop_prob=0.1
+        )
+        self.emb_enc = layers.EmbeddingEncoderLayer(
+            conv_layers=4, 
+            kernel_size=7,
+            filters=128, 
+            heads=8, 
+            enc_blocks=1,
+            drop_prob=drop_prob
+        )
+        self.qc_att = layers.CQAttentionLayer(
+            drop_prob=drop_prob
+        )
+        self.mod_enc = layers.ModelEncoderLayer(
+            conv_layers=2,
+            kernel_size=7,
+            filters=128,
+            heads=8,
+            enc_blocks=7,
+            drop_prob=drop_prob
+        )
+        self.out = layers.OutputLayer(
+            drop_prob=drop_prob
+        )
+
+    # def forward(self):
+

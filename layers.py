a25d27df46990423762c69d4fa141d386e35e176
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-09-18 17:45:56 +0530

    add InputEmbeddingLayer

--- layers.py
+++ layers.py
@@ -4,7 +4,14 @@ class InputEmbeddingLayer(nn.Module):
     """Input Embedding layer used by QANet
 
     Args:
-        nn ([type]): [description]
+        word_vectors (torch.Tensor): GLoVE vectors
+        hidden_size (int): Size of hidden states (p1)
     """
-    def __init__(self, ):
+    def __init__(self, word_vectors):
         super(InputEmbeddingLayer, self).__init__()
+        self.embedding = nn.Embedding.from_pretrained(word_vectors)
+
+    def forward(self, x):
+        emb = self.embedding(x) # (batch_size, sequence_length, p1)
+
+        return emb

b78be5c920d39f0653a50be66268f9e35e57c720
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-09-19 00:27:15 +0530

    add residual blocks and helper layers

--- layers.py
+++ layers.py
@@ -22,17 +22,16 @@ class InputEmbeddingLayer(nn.Module):
         return emb
 
 class EmbeddingEncodeLayer(nn.Module):
-    """[summary]
+    """Embedding Encoder Layer
 
     Args:
-        nn ([type]): [description]
+        d_model (int): Dimension of the word_vector
     """
-    def __init__(self, ):
+    def __init__(self, d_model):
         super(EmbeddingEncoderLayer, self).__init__()
-
+        self.enc_layer = EncoderBlock(d_model)
     def forward(self, x):
-
-        return out
+        return self.enc_layer(x)
 
 
 class CQAttentionLayer(nn.Module):
@@ -80,28 +79,108 @@ class OutputLayer(nn.Module):
 # ---------------- Helper Layers ----------------------        
 
 class SelfAttention(nn.Module):
-    """[summary]
+    """Multi-head self attention
+    Refer to Attention is all you need paper to understand terminology
 
     Args:
-        nn ([type]): [description]
+        d_model (int): dimension of the word vector
+        h (int): number of heads
     """
-    def __init__(self, ):
+    def __init__(self, d_model, h=8):
         super(SelfAttention, self).__init__()
-        
-    def forward(self, x):
+        self.d_model = d_model
+        self.h = h
+        self.d_v = int(self.d_model/h)
 
+        self.W_q = nn.Linear(in_features=self.d_v, out_features=self.d_v, bias=False)
+        self.W_k = nn.Linear(in_features=self.d_v, out_features=self.d_v, bias=False)
+        self.W_v = nn.Linear(in_features=self.d_v, out_features=self.d_v, bias=False)
+
+        self.linear = nn.Linear(in_features=self.d_model, out_features=self.d_model, bias=False)
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]  # Batch Size
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        # Split embedding in self.head pieces:
+        values = values.reshape(N, value_len, self.h, self.d_v)
+        keys = keys.reshape(N, key_len, self.h, self.d_v)
+        queries = query.reshape(N, query_len, self.h, self.d_v)
+
+        values = self.W_v(values)
+        keys = self.W_k(keys)
+        queries = self.W_q(queries)
+
+        energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys])
+        # queries shape: (N, query_len, heads, heads_dim)
+        # keys shape: (N, key_len, heads, heads_dim)
+        # energy shape: (N, heads, query_len, key_len)
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+
+        attention = torch.softmax(energy / (self.d_model ** (1/2)), dim=3)
+
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(N, query_len, self.h*self.d_v)
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape: (N, value_len, heads, heads_dim)
+        # after einsum: (N, query_len, heads, head_dim) then flatten last two dimensions
+
+        out = self.linear(out)
         return out
-
 
 class EncoderBlock(nn.Module):
-    """[summary]
+    """Encoder Block for QANet
+    convolution-layer × # + self-attention-layer + feed-forward-layer
 
     Args:
         nn ([type]): [description]
     """
-    def __init__(self, ):
+    def __init__(self, d_model, conv_layer=4, kernel_size=7, filters=128, heads=8):
         super(EmbeddingEncoderLayer, self).__init__()
 
+        # TODO positional encoding sometime
+
+        self.conv = nn.ModuleList(
+            [ConvolutionBlock(c_in=d_model,c_out=filters, kernel_size) for _ in range(conv_layer)] # FIXME is c_in right??
+        )
+        self.attn = SelfAttentionBlock(d_model)
+        self.ff = FeedForwardBlock(filters, filters)
+
     def forward(self, x):
-        
-        return out
+        x = self.conv(x)
+        x = self.attn(x)
+        x = self.ff(x)
+        return x
+
+# -------------------- Residual Blocks ----------------------
+
+class ConvolutionBlock(nn.Module):
+    def __init__(self, c_in, c_out, kernel_size):
+        super(ConvolutionBlock, self).__init__()
+        self.conv = nn.Conv2d(c_in, c_out, kernel_size, bias=False)
+        self.layer_norm = nn.LayerNorm() # FIXME add the layer norm parameters, what is normalised_shape
+    def forward(self, x):
+        return x + self.conv(self.layer_norm(x))
+
+class SelfAttentionBlock(nn.Module):
+    def __init__(self, d_model):
+        super(SelfAttentionBlock, self).__init__()
+        self.self_attn = SelfAttention(d_model)
+        self.layer_norm = nn.LayerNorm() # FIXME add the layer norm parameters, what is normalised_shape
+    def forward(self, x):
+        a = self.layer_norm(x)
+        return x + self.self_attn(a, a, a)
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(FeedForwardBlock, self).__init__()
+        self.ff = nn.Sequential(
+            nn.Linear(in_features, out_features, bias=False),
+            nn.ReLU()
+        )
+        self.layer_norm = nn.LayerNorm() # FIXME add the layer norm parameters, what is normalised_shape
+    def forward(self, x):
+        return x + self.ff(self.layer_norm(x))
+
+

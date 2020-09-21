688b60d78a7ff5c24f0f34acea44d5910a56b279
Sahil Khose <sahilkhose18@gmail.com>
2020-09-21 20:18:01 +0530

    Add comments and clean the code

--- layers.py
+++ layers.py
@@ -1,3 +1,9 @@
+"""Assortment of layers for use in models.py.
+
+Authors:
+    Sahil Khose (sahilkhose18@gmail.com)
+    Abhiraj Tiwari (abhirajtiwari@gmail.com)
+"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,20 +11,44 @@ import torch.nn.functional as F
 
 # --------------- Model Layers ------------------
 class InputEmbeddingLayer(nn.Module):
+    """Embedding layer used in QANet, without character-level embedding.
+    """
     def __init__(self, word_vectors, drop_prob):
+        """
+        @param word_vectors (torch.Tensor): Pre-trained word vectors.
+        @param drop_prob (float): Probability of zero-ing out activations.
+        """
         super(InputEmbeddingLayer, self).__init__()
 
         self.drop_prob = drop_prob 
         self.embed = nn.Embedding.from_pretrained(word_vectors)
         
     def forward(self, x):
-        emb = self.embed(x)   
-        emb = F.dropout(emb, self.drop_prob, self.training)
+        """Looks up word embeddings for the words in a batch of sentences.
+        @param x (torch.Tensor): (batch_size, sent_len)
+        @returns emb (torch.Tensor): Word embeddings for the batch of sentences. (batch_size, word_embed, sent_len)
+        """
+        emb = self.embed(x)  # (batch_size, word_embed, sent_len)
+        emb = F.dropout(emb, self.drop_prob, self.training)  # (batch_size, word_embed, sent_len)
         return emb  
 
 
 class EmbeddingEncoderLayer(nn.Module):
+    """Embedding Encoder layer which encodes using convolution, self attention and feed forward network.
+    Takes input from Input Embedding Layer.
+    """
     def __init__(self, conv_layers, kernel_size, filters, heads, enc_blocks, drop_prob, sent_len, word_embed, hidden_size):
+        """
+        @param conv_layers (int): Number of convolution layers in one Encoder Block.
+        @param kernel_size (int): Kernel size of the convolution layers.
+        @param filters (int): Number of filters for the convolution layers. 
+        @param heads (int): Number of heads for multihead attention. 
+        @param enc_blocks (int): Number of Encoder Blocks. 
+        @param drop_prob (float): Probability of zero-ing out activations.
+        @param sent_len (int): Input sentence size. 
+        @param word_embed (int): Pretrained word vector size. 
+        @param hidden_size (int): Number of features in the hidden state at each layer.
+        """
         super(EmbeddingEncoderLayer, self).__init__()
 
         self.emb_enc = nn.ModuleList([
@@ -30,23 +60,49 @@ class EmbeddingEncoderLayer(nn.Module):
         ])
 
     def forward(self, x):
+        """Encodes the word embeddings.
+        @param x (torch.Tensor): Word embeddings. (batch_size, word_embed, sent_len)
+        @returns x (torch.Tensor): Encoded Word embeddings. (batch_size, hidden_size, sent_len)
+        """
         for layer in self.emb_enc:
-            x = layer(x)
+            x = layer(x)  # (batch_size, hidden_size, sent_len)
         return x
 
 
 class CQAttentionLayer(nn.Module):
+    """Context Query Attention Layer.
+    Takes 2 inputs: Context Encoded Embedding and Question Encoded Embedding. 
+    """
     def __init__(self, drop_prob):
+        """
+        @param drop_prob (float): Probability of zero-ing out activations.
+        """
         super(CQAttentionLayer, self).__init__()
 
     def forward(self, context, question):
-        return context
+        """
+        @param context (torch.Tensor): Encoded context embedding. (batch_size, hidden_size, c_len)
+        @param question (torch.Tensor): Encoded question embedding. (batch_size, hidden_size, q_len)
+        """
+        return context  # (batch_size, hidden_size, c_len)
 
 
 class ModelEncoderLayer(nn.Module):
-    """
+    """Model Encoder layer which encodes using convolution, self attention and feed forward network.
+    Takes input from CQAttention Layer.
     """
     def __init__(self, conv_layers, kernel_size, filters, heads, enc_blocks, drop_prob, sent_len, word_embed, hidden_size):
+        """
+        @param conv_layers (int): Number of convolution layers in one Encoder Block.
+        @param kernel_size (int): Kernel size of the convolution layers.
+        @param filters (int): Number of filters for the convolution layers. 
+        @param heads (int): Number of heads for multihead attention. 
+        @param enc_blocks (int): Number of Encoder Blocks. 
+        @param drop_prob (float): Probability of zero-ing out activations.
+        @param sent_len (int): Input sentence size. 
+        @param word_embed (int): Word vector size. 
+        @param hidden_size (int): Number of features in the hidden state at each layer.
+        """
         super(ModelEncoderLayer, self).__init__()
 
         self.model_enc = nn.ModuleList([
@@ -58,117 +114,204 @@ class ModelEncoderLayer(nn.Module):
         ])
 
     def forward(self, x):
+        """Encodes the word vectors.
+        @param x (torch.Tensor): Input word vectors from CQAttention. (batch_size, , sent_len)
+        @returns x (torch.Tensor): Encoded Word Vectors. (batch_size, hidden_size, sent_len)
+        """
         for layer in self.model_enc:
-            x = layer(x)
+            x = layer(x)  # (batch_size, hidden_size, sent_len)
         return x
 
 
 class OutputLayer(nn.Module):
+    """Output Layer which outputs the probability distribution for the answer span in the context span.
+    Takes inputs from 2 Model Encoder Layers.
+    """
     def __init__(self, drop_prob, word_embed):
+        """
+        @param drop_prob (float): Probability of zero-ing out activations.
+        @param word_embed (int): Word vector size.
+        """
         super(OutputLayer, self).__init__()
         self.ff = nn.Linear(2*word_embed, 1)
         self.soft = nn.Softmax(dim=2)
 
     def forward(self, input_1, input_2):
-        # (batch_size, word_embed, sent_len)
-        x = torch.cat((input_1, input_2), dim=1) 
-        x = self.ff(x.permute(0, 2, 1)).permute(0, 2, 1)
-        x = self.soft(x)
-        return x
+        """Encodes the word embeddings.
+        @param input_1 (torch.Tensor): Word vectors from first Model Encoder Layer. (batch_size, hidden_size, sent_len)
+        @param input_2 (torch.Tensor): Word vectors from second Model Encoder Layer. (batch_size, hidden_size, sent_len)
+        @returns p (torch.Tensor): Probability distribution for start/end token. (batch_size, sent_len)
+        """
+        x = torch.cat((input_1, input_2), dim=1)  # (batch_size, 2*hidden_size, sent_len)
+        x = self.ff(x.permute(0, 2, 1)).permute(0, 2, 1)  # (batch_size, 1, sent_len)
+        p = self.soft(x)  # (batch_size, 1, sent_len)
+        return p.squeeze()  # (batch_size, sent_len)
 
 
 # ---------------- Helper Layers ----------------------
-class SelfAttention(nn.Module):
-    def __init__(self, word_embed, sent_len, heads, drop_prob):
-        super(SelfAttention, self).__init__()
-
-    def forward(self, x):
-        return x     
-
-
 class EncoderBlock(nn.Module):
+    """Encoder Block used in Input Embedding Layer and Model Embedding Layer. 
+    """
     def __init__(self, conv_layers, kernel_size, filters, heads, drop_prob, sent_len, word_embed, hidden_size):
+        """
+        @param conv_layers (int): Number of convolution layers in one Encoder Block.
+        @param kernel_size (int): Kernel size of the convolution layers.
+        @param filters (int): Number of filters for the convolution layers. 
+        @param heads (int): Number of heads for multihead attention.
+        @param drop_prob (float): Probability of zero-ing out activations.
+        @param sent_len (int): Input sentence size. 
+        @param word_embed (int): Word vector size. 
+        @param hidden_size (int): Number of features in the hidden state at each layer.
+        """
         super(EncoderBlock, self).__init__()
 
         self.pos_enc = nn.Embedding(sent_len, word_embed)
 
         self.conv = nn.ModuleList([
             ConvBlock(word_embed=word_embed, sent_len=sent_len,
-                        out_channels=hidden_size, kernel_size=kernel_size),
+                        hidden_size=hidden_size, kernel_size=kernel_size),
             *(ConvBlock(word_embed=hidden_size, sent_len=sent_len,
-                      out_channels=hidden_size, kernel_size=kernel_size)
+                      hidden_size=hidden_size, kernel_size=kernel_size)
             for _ in range(conv_layers - 1))
         ])
         self.att = SelfAttentionBlock(
-            word_embed=hidden_size, sent_len=sent_len, heads=heads, drop_prob=drop_prob)
+            hidden_size=hidden_size, sent_len=sent_len, heads=heads, drop_prob=drop_prob)
         self.ff = FeedForwardBlock(
-            word_embed=hidden_size, sent_len=sent_len, in_features=hidden_size, out_features=hidden_size)
+            hidden_size=hidden_size, sent_len=sent_len)
 
     def forward(self, x):
-        N, word_embed, sent_len = x.shape
-        positions = torch.arange(0, sent_len).expand(N, sent_len)
+        """Encodes the word vectors.
+        @param x (torch.Tensor): Word vectors. (batch_size, word_embed, sent_len)
+        @returns x (torch.Tensor): Encoded Word embeddings. (batch_size, hidden_size, sent_len)
+        """
+        N, word_embed, sent_len = x.shape  # N: batch_sizes
+        positions = torch.arange(0, sent_len).expand(N, sent_len) 
 
         # Add positional Encoding:
-        x = x.permute(0, 2, 1) + self.pos_enc(positions)
-        x = x.permute(0, 2, 1)
+        x = x.permute(0, 2, 1) + self.pos_enc(positions)  # (batch_size, word_embed, sent_len)
+        x = x.permute(0, 2, 1)  # (batch_size, word_embed, sent_len)
 
         # Layer_norm -> conv 
         for layer in self.conv:
-            x = layer(x)
+            x = layer(x)  # (batch_size, hidden_size, sent_len)
 
         # Layer_norm -> Self Attention
-        x = self.att(x)
+        x = self.att(x)  # (batch_size, hidden_size, sent_len)
         # Layer_norm -> Feed Forward
-        x = self.ff(x)
+        x = self.ff(x)  # (batch_size, hidden_size, sent_len)
+        return x
+
+
+class SelfAttention(nn.Module):
+    """Self Attention used in Self Attention Block for Encoder Block.
+    """
+
+    def __init__(self, hidden_size, sent_len, heads, drop_prob):
+        """
+        @param word_embed (int): Word vector size. 
+        @param sent_len (int): Input sentence size. 
+        @param heads (int): Number of heads for multihead attention. 
+        @param drop_prob (float): Probability of zero-ing out activations.
+        """
+        super(SelfAttention, self).__init__()
+
+    def forward(self, x):
+        """
+        @param x (torch.Tensor): Word vectors. (batch_size, hidden_size, sent_len)
+        @returns x (torch.Tensor): Word vectors with self attention. (batch_size, hidden_size, sent_len)
+        """
         return x
 
 
 # ---------------- Helper Residual Blocks ----------------------
 class ConvBlock(nn.Module):
-    def __init__(self, word_embed, sent_len, out_channels, kernel_size):
+    """Conv Block used in Encoder Block.
+    """
+    def __init__(self, word_embed, sent_len, hidden_size, kernel_size):
+        """
+        @param word_embed (int): Word vector size. 
+        @param sent_len (int): Input sentence size. 
+        @param out_channels (int): Number of output features.
+        @param kernel_size (int): Kernel size of the convolution layers.
+        """
         super(ConvBlock, self).__init__()
         self.word_embed = word_embed
-        self.out_channels = out_channels
+        self.hidden_size = hidden_size
 
         self.layer_norm = nn.LayerNorm([word_embed, sent_len])
-        self.conv = nn.Conv1d(word_embed, out_channels,
-                              kernel_size, padding=kernel_size//2)
-        self.w_s = nn.Linear(word_embed, out_channels) # linear projection, read identity mapping(3.2 section) in ResNet paper
+        self.conv = nn.Conv1d(word_embed, hidden_size, kernel_size, padding=kernel_size//2)
+        self.w_s = nn.Linear(word_embed, hidden_size) # linear projection
 
     def forward(self, x):
-        x_l = self.layer_norm(x)
-        if(self.word_embed != self.out_channels): 
-            x = self.w_s(x.permute(0, 2, 1)).permute(0, 2, 1)
-        x = x + self.conv(x_l)
-        return x
+        """
+        @param x (torch.Tensor): Word vectors. (batch_size, word_embed, sent_len)
+        @returns x (torch.Tensor): Word vectors. (batch_size, hidden_size, sent_len)
 
+        Shortcut Connections based on the paper:
+        "Deep Residual Learning for Image Recognition"
+        by Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+        (https://arxiv.org/abs/1512.03385)
 
-class FeedForwardBlock(nn.Module):
-    def __init__(self, word_embed, sent_len, in_features, out_features):
-        super(FeedForwardBlock, self).__init__()
+        For linear projection before Shortcut Connection:
+            Refer Equation 2 (Section 3.2) in https://arxiv.org/pdf/1512.03385.pdf 
 
-        self.layer_norm = nn.LayerNorm([word_embed, sent_len])
-        self.ff = nn.Sequential(
-            nn.Linear(in_features, out_features),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        x_l = self.layer_norm(x)
-        x = x + self.ff(x.permute(0, 2, 1)).permute(0, 2, 1)
+        """
+        x_l = self.layer_norm(x)  # (batch_size, word_embed, sent_len)
+        if(self.word_embed != self.hidden_size): 
+            x = self.w_s(x.permute(0, 2, 1)).permute(0, 2, 1)  # (batch_size, hidden_size, sent_len)
+        x = x + self.conv(x_l)  # (batch_size, hidden_size, sent_len)
         return x
 
 
 class SelfAttentionBlock(nn.Module):
-    def __init__(self, word_embed, sent_len, heads, drop_prob):
+    """Self Attention Block used in Encoder Block.
+    """
+    def __init__(self, hidden_size, sent_len, heads, drop_prob):
+        """
+        @param word_embed (int): Word vector size. 
+        @param sent_len (int): Input sentence size. 
+        @param heads (int): Number of heads for multihead attention.
+        @param drop_prob (float): Probability of zero-ing out activations.
+        """
         super(SelfAttentionBlock, self).__init__()
 
-        self.layer_norm = nn.LayerNorm([word_embed, sent_len])
-        self.att = SelfAttention(word_embed=word_embed, sent_len=sent_len, heads=heads, drop_prob=drop_prob)
+        self.layer_norm = nn.LayerNorm([hidden_size, sent_len])
+        self.att = SelfAttention(hidden_size=hidden_size, sent_len=sent_len, heads=heads, drop_prob=drop_prob)
 
     def forward(self, x):
-        x_l = self.layer_norm(x)
-        x = x + self.att(x_l)
+        """
+        @param x (torch.Tensor): Word vectors. (batch_size, hidden_size, sent_len)
+        @returns x (torch.Tensor): Word vectors with self attention. (batch_size, hidden_size, sent_len)
+        """
+        x_l = self.layer_norm(x)  # (batch_size, hidden_size, sent_len)
+        x = x + self.att(x_l)  # (batch_size, hidden_size, sent_len)
+        return x
+
+
+class FeedForwardBlock(nn.Module):
+    """Feed Forward Block used in Encoder Block.
+    """
+
+    def __init__(self, hidden_size, sent_len):
+        """
+        @param word_embed (int): Word vector size. 
+        @param sent_len (int): Input sentence size.
+        """
+        super(FeedForwardBlock, self).__init__()
+
+        self.layer_norm = nn.LayerNorm([hidden_size, sent_len])
+        self.ff = nn.Sequential(
+            nn.Linear(in_features=hidden_size, out_features=hidden_size),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        """
+        @param x (torch.Tensor): Word vectors. (batch_size, hidden_size, sent_len)
+        @returns x (torch.Tensor): Word vectors. (batch_size, hidden_size, sent_len)
+        """
+        x_l = self.layer_norm(x)  # (batch_size, hidden_size, sent_len)
+        x = x + self.ff(x_l.permute(0, 2, 1)).permute(0, 2, 1)  # (batch_size, hidden_size, sent_len)
         return x
 
 
--- models.py
+++ models.py
@@ -1,3 +1,9 @@
+"""Top-level model classes.
+
+Authors:
+    Sahil Khose (sahilkhose18@gmail.com)
+    Abhiraj Tiwari (abhirajtiwari@gmail.com)
+"""
 import layers
 import torch
 import torch.nn as nn
@@ -5,9 +11,30 @@ import torch.nn.functional as F
 
 
 class QANET(nn.Module):
-    """Implementation of QANET model for SQuAD 2.0
+    """QANet model for SQuAD 2.0
+
+    Based on the paper:
+    "QANet: Combining Local Convolution with Global Self-Attention for Reading Comprehension"
+    by Adams Wei Yu, David Dohan, Minh-Thang Luong, Rui Zhao, Kai Chen, Mohammad Norouzi, Quoc V. Le
+    (https://arxiv.org/abs/1804.09541).
+
+    Follows a high-level structure commonly found in SQuAD 2.0 models:
+        - Input Embedding Layer: Embed word indices to get word vectors.
+        - Embedding Encoder Layer: Encode the embedded sequence.
+        - Context-Query Attention Layer: Apply a context-query attention mechanism to the encoded sequence.
+        - Model Encoder Layer: Encode the sequence again.
+        - Output Layer: Simple layer (e.g., fc + softmax) to get final outputs.   
     """
-    def __init__(self, word_vectors, hidden_size=128, drop_prob=0., context_len=1, question_len=1, word_embed=300):
+    def __init__(self, word_vectors, hidden_size=128, drop_prob=0., c_len=1, q_len=1, word_embed=300):
+        """Init QANET Model.
+        
+        @param word_vectors (torch.Tensor): Pre-trained word vectors.
+        @param hidden_size (int): Number of features in the hidden state at each layer.
+        @param drop_prob (float): Dropout probability.
+        @param c_len (int): Context sentence length. 
+        @param q_len (int): Question sentence length. 
+        @param word_embed (int): Pretrained word vector size. 
+        """
         super(QANET, self).__init__()
         self.c_emb = layers.InputEmbeddingLayer(
             word_vectors=word_vectors,
@@ -24,7 +51,7 @@ class QANET(nn.Module):
             heads=8, 
             enc_blocks=1,
             drop_prob=drop_prob,
-            sent_len=context_len, 
+            sent_len=c_len, 
             word_embed=word_embed,
             hidden_size=hidden_size
         )
@@ -35,7 +62,7 @@ class QANET(nn.Module):
             heads=8,
             enc_blocks=1,
             drop_prob=drop_prob,
-            sent_len=question_len,
+            sent_len=q_len,
             word_embed=word_embed,
             hidden_size=hidden_size
         )
@@ -49,33 +76,47 @@ class QANET(nn.Module):
             heads=8,
             enc_blocks=7,
             drop_prob=drop_prob, 
-            sent_len=context_len,
-            word_embed=hidden_size, # new dim: [c, a, c.a, c.b]: qc_att.shape[1]
+            sent_len=c_len,
+            word_embed=hidden_size,  # FIXME new dim: [c, a, c.a, c.b]: qc_att.shape[1]
             hidden_size=hidden_size
         )
         self.start_out = layers.OutputLayer(
             drop_prob=drop_prob,
-            word_embed=hidden_size # new dim: model_enc.shape[1]
+            word_embed=hidden_size  # FIXME new dim: model_enc.shape[1], I think this is right
         )
         self.end_out = layers.OutputLayer(
             drop_prob=drop_prob,
-            word_embed=hidden_size  # new dim: model_enc.shape[1]
+            word_embed=hidden_size  # FIXME new dim: model_enc.shape[1] 
         )
 
-    def forward(self, context, question, answer):
-        # c_emb = self.c_emb(context)
-        # q_emb = self.q_emb(question
-        c_emb_enc = self.c_emb_enc(context)
-        q_emb_enc  = self.q_emb_enc(question)
-        qc_att = self.qc_att(c_emb_enc, q_emb_enc)
-        mod_enc_1 = self.mod_enc(qc_att)
-        mod_enc_2 = self.mod_enc(mod_enc_1)
-        mod_enc_3 = self.mod_enc(mod_enc_2)
-        start_out = self.start_out(mod_enc_1, mod_enc_2)
-        end_out = self.end_out(mod_enc_1, mod_enc_3)
-        score = start_out * end_out # some kind of probability distribution multiplication
-        # return (start_out, end_out)
-        return score
+    def forward(self, context, question):
+        """ Take a mini-batch of context and question sentences, compute the log-likelihood of each 
+        position in the context being the start or end of an answer span. 
+        
+
+        @param context (List[List[str]]): List of context sentence tokens.
+        @param question (List[List[str]]): List of question sentence tokens.
+        @param answer (List[List[str]]): List of answer sentence tokens.
+
+        @returns start_out (Tensor): Start probability distribution.
+        @returns end_out (Tensor): End probability distribution.
+        """
+        # c_emb = self.c_emb(context)  # (batch_size, word_embed, c_len)
+        # q_emb = self.q_emb(question)  # (batch_size, word_embed, q_len)
+
+        c_emb_enc = self.c_emb_enc(context)  # (batch_size, hidden_size, c_len)
+        q_emb_enc  = self.q_emb_enc(question)  # (batch_size, hidden_size, q_len)
+
+        qc_att = self.qc_att(c_emb_enc, q_emb_enc)  # (batch_size, , c_len)
+
+        mod_enc_1 = self.mod_enc(qc_att)  # (batch_size, hidden_size, c_len)
+        mod_enc_2 = self.mod_enc(mod_enc_1)  # (batch_size, hidden_size, c_len)
+        mod_enc_3 = self.mod_enc(mod_enc_2)  # (batch_size, hidden_size, c_len)
+
+        start_out = self.start_out(mod_enc_1, mod_enc_2)  # (batch_size, c_len)
+        end_out = self.end_out(mod_enc_1, mod_enc_3)  # (batch_size, c_len)
+
+        return start_out, end_out
 
 
 if __name__ == "__main__":
@@ -83,12 +124,11 @@ if __name__ == "__main__":
 
     context = torch.randn((32, 300, 200))
     question = torch.randn((32, 300, 100))
-    answer = torch.randn((32, 300, 150)) # part of context
+    # answer = torch.randn((32, 300, 150))  # part of context
 
     word_vectors = torch.randn((2, 3))
 
     qanet = QANET(word_vectors, hidden_size=128,
-                  drop_prob=0., context_len=200, question_len=100, word_embed=300)
-    # print(qanet(context, question, answer)[1].shape)
+                  drop_prob=0., c_len=200, q_len=100, word_embed=300)
     print("Final score shape:")
-    print(qanet(context, question, answer).shape)
+    print(qanet(context, question)[0].shape)

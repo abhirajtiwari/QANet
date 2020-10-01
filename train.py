4c15400ef2018b61c7965bfe99cfda04e7316991
Abhiraj Tiwari <abhirajtiwari@gmail.com>
2020-10-01 13:55:17 +0530

    replace bidaf with qanet

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ import util
 from args import get_train_args
 from collections import OrderedDict
 from json import dumps
-from models import BiDAF
+from models import QANet
 from tensorboardX import SummaryWriter
 from tqdm import tqdm
 from ujson import load as json_load
@@ -46,9 +46,10 @@ def main(args):
 
     # Get model
     log.info('Building model...')
-    model = BiDAF(word_vectors=word_vectors,
-                  hidden_size=args.hidden_size,
-                  drop_prob=args.drop_prob)
+    model = QANet(word_vectors=word_vectors,hidden_state=128, d_model=300)  
+    # model = BiDAF(word_vectors=word_vectors,
+    #               hidden_size=args.hidden_size,
+    #               drop_prob=args.drop_prob)
     model = nn.DataParallel(model, args.gpu_ids)
     if args.load_path:
         log.info(f'Loading checkpoint from {args.load_path}...')
